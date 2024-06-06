a3db9bb5f42e2146c508ae4d62be5431cb462629
valikoz <makharynskyi.vy@gmail.com>
2024-06-06 14:46:15 +0300

    Remove lazy autodetection

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -29,4 +29,9 @@ require("lazy").setup({
       },
     },
   },
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = true,
+    notify = false, -- get a notification when changes are found
+  },
 } --[[@as LazyConfig]])
